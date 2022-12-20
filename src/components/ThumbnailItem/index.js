6b63bfa84c1084e3ca42def4cdd8a17760ee6548
Saiteja <saitejamacha123@gmail.com>
2022-12-20 12:56:03 +0000

    Gallery-App

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,35 @@
+import './index.css'
+
+// Sai Tej's Code
+
+const ThumbnailItem = props => {
+  const {thumbnailDetails, updateImage, isActiveThumb} = props
+  const {
+    id,
+    imageUrl,
+    thumbnailUrl,
+    imageAltText,
+    thumbnailAltText,
+  } = thumbnailDetails
+
+  const selectImage = () => {
+    updateImage(id)
+  }
+
+  const thumbImgVisibleStyle = isActiveThumb && 'visible-thubnail'
+
+  return (
+    <li className="li-items">
+      <button>
+        <img
+          alt={thumbnailAltText}
+          className={`thumb-images ${thumbImgVisibleStyle}`}
+          onClick={selectImage}
+          src={thumbnailUrl}
+        />
+      </button>
+    </li>
+  )
+}
+
+export default ThumbnailItem
